d222b06154e01bad1772916b8970455641709301
t_tamir <t_tamir@126.com>
2017-11-02 20:06:27 +0800

    the finish code

--- tutorial/spiders/dmoz_spider.py
+++ tutorial/spiders/dmoz_spider.py
@@ -1,5 +1,7 @@
 import scrapy
 
+from tutorial.items import DmozItem 
+
 class DmozSpider(scrapy.Spider):
     name = "dmoz"
     allowed_domains = ['dmoztools.net']
@@ -10,8 +12,14 @@ class DmozSpider(scrapy.Spider):
     def parse(self, response):
         sel = scrapy.selector.Selector(response)
         sites = sel.xpath('//div[@class="title-and-desc"]')
+        
+        items = []
         for site in sites:
-            title = site.xpath('a/div/text()').extract()
-            link = site.xpath('a/@href').extract()
-            desc = site.xpath('div/text()').extract()
-            print(title, link, desc)
+            item = DmozItem()
+            item['title'] = site.xpath('a/div/text()').extract()
+            item['link'] = site.xpath('a/@href').extract()
+            item['desc'] = site.xpath('div/text()').extract()
+            #print(title, link, desc)
+            items.append(item)
+
+        return items
